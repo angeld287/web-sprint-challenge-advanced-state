3d9d8f5793a8b9ab76091d391a46812658685488
Angel Angeles <daniel_1234@hotmail.es>
2023-10-21 12:47:13 -0400

    configuring reducer

--- frontend/state/reducer.js
+++ frontend/state/reducer.js
@@ -1,24 +1,64 @@
 // ❗ You don't need to add extra reducers to achieve MVP
 import { combineReducers } from 'redux'
+import { MOVE_CLOCKWISE, MOVE_COUNTERCLOCKWISE, SET_QUIZ_INTO_STATE, SET_SELECTED_ANSWER, SET_INFO_MESSAGE, INPUT_CHANGE, RESET_FORM } from './action-types';
 
 const initialWheelState = 0
 function wheel(state = initialWheelState, action) {
-  return state
+  switch (action.type) {
+    case MOVE_CLOCKWISE:
+      return {
+        ...state,
+        wheel: state.wheel + 1
+      }
+
+    case MOVE_COUNTERCLOCKWISE:  
+      return {
+        ...state,
+        wheel: state.wheel - 1
+      }
+  
+    default:
+      return state;
+  }
 }
 
 const initialQuizState = null
 function quiz(state = initialQuizState, action) {
-  return state
+  switch (action.payload) {
+    case SET_QUIZ_INTO_STATE:
+      return {
+        ...state,
+        quiz: state.payload
+      }
+    default:
+      return state;
+  }
 }
 
 const initialSelectedAnswerState = null
 function selectedAnswer(state = initialSelectedAnswerState, action) {
-  return state
+  switch (action.payload) {
+    case SET_SELECTED_ANSWER:
+      return {
+        ...state,
+        selectedAnswer: action.payload
+      }
+    default:
+      return state;
+  }
 }
 
 const initialMessageState = ''
 function infoMessage(state = initialMessageState, action) {
-  return state
+  switch (action.payload) {
+    case SET_INFO_MESSAGE:
+      return {
+        ...state,
+        infoMessage: action.payload
+      }
+    default:
+      return state;
+  }
 }
 
 const initialFormState = {
@@ -27,7 +67,22 @@ const initialFormState = {
   newFalseAnswer: '',
 }
 function form(state = initialFormState, action) {
-  return state
+  switch (action.type) {
+    case INPUT_CHANGE:
+      return {
+        ...state,
+        form: action.payload
+      }
+
+    case RESET_FORM:  
+      return {
+        ...state,
+        form: initialFormState
+      }
+  
+    default:
+      return state;
+  }
 }
 
 export default combineReducers({ wheel, quiz, selectedAnswer, infoMessage, form })
